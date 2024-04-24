9b8cd2f46f403b95f04209b1de7ca2b89738b64e
GreeningSiren <greeningsiren@gmail.com>
2024-04-24 09:49:59 +0300

    Update app.js

--- _js/app.js
+++ _js/app.js
@@ -28,7 +28,7 @@ $(document).ready(function() {
   AOS.init({
     disable: 'mobile',
     offset: 200,
-    duration: 100,
+    duration: 50,
     easing: 'ease-in-sine',
     delay: 100,
     once: true
